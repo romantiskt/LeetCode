ecad4290e55a1e50ce916e82fa5c67b90d8680c3
王阳 <wyroman@163.com>
2019-06-04 18:50:06 +0800

    init

--- app/src/main/java/com/rolan/leetcode/MainActivity.java
+++ app/src/main/java/com/rolan/leetcode/MainActivity.java
@@ -3,6 +3,13 @@ package com.rolan.leetcode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.rolan.leetcode.easy.Chapter268;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btn_268})
+    void click(View view) {
+        int[] arr = {0,1, 2, 4, 5, 6};
+        int result = Chapter268.doMath(arr);
+        Log.d("wang", String.valueOf(result));
     }
 }
