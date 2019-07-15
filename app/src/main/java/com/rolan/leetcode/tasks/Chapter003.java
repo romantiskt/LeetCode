74b18d82abc18fffe3cca30c9b9b689fbf9db71e
王阳 <wyroman@163.com>
2019-07-15 16:29:42 +0800

    无重复字符的最长子串

--- app/src/main/java/com/rolan/leetcode/tasks/Chapter003.java
+++ app/src/main/java/com/rolan/leetcode/tasks/Chapter003.java
@@ -2,7 +2,9 @@ package com.rolan.leetcode.tasks;
 
 import com.rolan.leetcode.IEngine;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -32,6 +34,24 @@ public class Chapter003 implements IEngine {
         }
 
         return maxLen;
+    }
+
+    /**
+     * 经典解法 当存在相同字符时 马上就会将前置角标跳到相同字符的区间
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring2(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int j = 0, i = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1);
+        }
+        return ans;
     }
 
     /**
